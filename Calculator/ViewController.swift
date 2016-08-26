264314792003adf45f7e272ef6980fde91776454
Austin Wood <austin@austinkwood.com>
2016-08-26 22:00:00 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Austin Wood on 2016-08-26.
+//  Copyright © 2016 Austin Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
