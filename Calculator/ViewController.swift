7ddd2b21a3b8ed4d703d19604f253b5f8d0e43ac
Austin Wood <austin@austinkwood.com>
2016-08-28 16:07:07 +0300

    Calculator working properly for Integers

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,181 +10,104 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var outputLbl: UILabel!
     
-    
-    var runningNumber = String()
-    var leftString = String()
-    var rightString = String()
-    
-    
-    enum Operation {
-        case Divide
-        case Multiply
-        case Subtract
-        case Add
-        case Equals
-    }
-    
-    var currentOperation = Operation.Equals
-    
+    var runningNumber = ""
+    var leftString = ""
+    var rightString = ""
+    var currentOperation = 0
+    var equalsPressed = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewWillAppear(animated: Bool) {
-        
         super.viewWillAppear(animated)
         self.navigationController?.navigationBarHidden = true
-        
         resetCalc()
     }
     
-    
-    
-    
     func resetCalc() {
-        
         runningNumber = ""
         leftString = ""
         rightString = ""
-        
+        currentOperation = 0
+        equalsPressed = false
         outputLbl.text = "0"
     }
-    
     
     @IBAction func clearPressed(sender: AnyObject) {
         resetCalc()
     }
     
+    @IBAction func numberPressed(sender: AnyObject) {
+        
+        if equalsPressed {
+            resetCalc()
+        }
+        
+        let number = "\(sender.tag)"
+        runningNumber = runningNumber + number
+        outputLbl.text = runningNumber
+    }
     
-
+    @IBAction func operationPressed(sender: AnyObject) {
+        
+        if equalsPressed {
+            equalsPressed = false
+        } else if currentOperation == 0 {
+            leftString = runningNumber
+        } else {
+            rightString = runningNumber
+            processOperation()
+        }
+        
+        runningNumber = ""
+        currentOperation = sender.tag
+    }
     
     
-    /////////////////////////////
-    ///// OPERATION PRESSED /////
-    /////////////////////////////
+    @IBAction func equalsPressed(sender: AnyObject) {
+        
+        if !equalsPressed {
+            equalsPressed = true
+            rightString = runningNumber
+            runningNumber = ""
+        }
+        
+        processOperation()
+    }
     
     
     func processOperation() {
         
         var calculation = Int()
         
+        if leftString == "" {
+            leftString = "0"
+        }
+        
         switch currentOperation {
-        case .Divide:
-            
-            calculation = Int(leftString)! / Int(runningNumber)!
+        
+        case 1:
+            calculation = Int(leftString)! / Int(rightString)!
             break
-            
-        case .Multiply:
-            
-            calculation = Int(leftString)! * Int(runningNumber)!
+        case 2:
+            calculation = Int(leftString)! * Int(rightString)!
             break
-            
-        case .Subtract:
-            
-            calculation = Int(leftString)! - Int(runningNumber)!
+        case 3:
+            calculation = Int(leftString)! - Int(rightString)!
             break
-            
-        case .Add:
-            
-            calculation = Int(leftString)! + Int(runningNumber)!
+        case 4:
+            calculation = Int(leftString)! + Int(rightString)!
             break
-            
-        case .Equals:
-            
-            ////// RESUME HERE
-            ////// EQUALS SOMETIMES BREAKS THE CODE
-            calculation = Int(runningNumber)!
+        default:
             break
         }
         
         leftString = "\(calculation)"
-        runningNumber = ""
-        outputLbl.text = "\(calculation)"
-        
+        outputLbl.text = leftString
     }
-    
-    
-    
-    @IBAction func dividePressed(sender: AnyObject) {
-        processOperation()
-        currentOperation = .Divide
-    }
-    
-    @IBAction func multiplyPressed(sender: AnyObject) {
-        processOperation()
-        currentOperation = .Multiply
-    }
-    
-    @IBAction func subtractPressed(sender: AnyObject) {
-        processOperation()
-        currentOperation = .Subtract
-    }
-    
-    @IBAction func addPressed(sender: AnyObject) {
-        processOperation()
-        currentOperation = .Add
-    }
-    
-    @IBAction func equalsPressed(sender: AnyObject) {
-        processOperation()
-        currentOperation = .Equals
-    }
-    
-    //////////////////////////
-    ///// NUMBER PRESSED /////
-    //////////////////////////
-    
-    
-    func numberPressed(number: String) {
-        print(number)
-        runningNumber = runningNumber + number
-        outputLbl.text = runningNumber
-    }
-    
-    
-    @IBAction func zeroPressed(sender: AnyObject) {
-        numberPressed("0")
-    }
-    
-    @IBAction func onePressed(sender: AnyObject) {
-        numberPressed("1")
-    }
-    
-    @IBAction func twoPressed(sender: AnyObject) {
-        numberPressed("2")
-    }
-    
-    @IBAction func threePressed(sender: AnyObject) {
-        numberPressed("3")
-    }
-    
-    @IBAction func fourPressed(sender: AnyObject) {
-        numberPressed("4")
-    }
-    
-    @IBAction func fivePressed(sender: AnyObject) {
-        numberPressed("5")
-    }
-    
-    @IBAction func sixPressed(sender: AnyObject) {
-        numberPressed("6")
-    }
-    
-    @IBAction func sevenPressed(sender: AnyObject) {
-        numberPressed("7")
-    }
-    
-    @IBAction func eightPressed(sender: AnyObject) {
-        numberPressed("8")
-    }
-    
-    @IBAction func ninePressed(sender: AnyObject) {
-        numberPressed("9")
-    }
-    
     
 }
